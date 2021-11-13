74057d24fda0e778dba20eea0dc0582a753c1892
Almas Mirzandi <88608985+b1euuuuwu@users.noreply.github.com>
2021-11-13 09:48:02 +0700

    Add files via upload
    Kerangka Projectnya :D

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+struct studentData
+{
+	int ID;
+	int Age;
+	int GPA;
+	string Class;
+	string Name;
+};
+
+studentData student[99];
+
+int main()
+{
+	
+	cout << "Welcome to our student data management system!" << endl << endl
+		 << "What would you like to do?" << endl;
+
+	/*
+		1. Add student data
+		2. Display student data
+		   a. Opt 1: Sort by name (alphabetically)
+		   b. Opt 2: Sort by Age (output choice: ascending or descending?)
+		   c. Opt 3: Display unsorted data
+		3. Search for student data with variable "ID" as input
+	*/
+}
