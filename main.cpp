78a8239ca50b4f6e589c0b0abe8881d75ce4b6d6
Almas Mirzandi <88608985+b1euuuuwu@users.noreply.github.com>
2021-11-15 07:27:53 +0700

    Added "add_student" function

--- main.cpp
+++ main.cpp
@@ -11,15 +11,45 @@ struct studentData
 };
 
 studentData student[99];
+int counter = 0; //array index
+
+void add_student()
+{
+	addStudent: //goto Label
+	cout << "Please enter the following: " << endl;
+	cout << "Student ID: "; cin >> student[counter].ID;
+	cout << endl;
+	cout << "Age: "; cin >> student[counter].Age;
+	cout << endl;
+	cout << "GPA: "; cin >> student[counter].GPA;
+	cout << endl;
+	cout << "Class: "; cin >> student[counter].Class;
+	cout << endl;
+	cout << "Student Name: "; cin >> student[counter].Name;
+	cout << endl << endl;
+
+	char more;
+
+	cout << "Would you like to add more student data?(y/n) "; cin >> more;
+
+	while (more == 'y')
+	{
+		cout << endl << endl;
+		counter++;
+		goto addStudent;
+	}
+}
 
 int main()
 {
-	
+	start:
+	int choice;
+
 	cout << "Welcome to our student data management system!" << endl << endl
 		 << "What would you like to do?" << endl;
 
 	/*
-		Make fucntions to do these tasks:
+		Make functions to do these tasks:
 		1. Add student data
 		2. Display student data (Joy and Katarina)
 		   a. Opt 1: Sort by name (alphabetically)
@@ -27,4 +57,24 @@ int main()
 		   c. Opt 3: Display unsorted data
 		3. Search for student data with variable "ID" as input (tugas Almas)
 	*/
+
+	switch(choice)
+	{
+		case 1:
+			add_student();
+			goto start;
+		break;
+		case 2:
+		/*
+			function for sort algorithm
+		*/
+		case 3:
+		/*
+			function for search algorithm
+		*/
+		case 4:
+		goto exit;
+	}
+	exit:
+	return 0;
 }
